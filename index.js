134f4ef138f40a201536958d4040acb0eb1364e6
somerongit <someron.bakuli05@gmail.com>
2021-09-29 09:39:04 +0530

    Mongoose Schema Update

--- index.js
+++ index.js
@@ -13,19 +13,32 @@ connect.then((db) => {
         name: 'Uthappizza',
         description: 'test'
     })
-    .then((dish) => {
-        console.log(dish);
-        return Dishes.find({}).exec();
-    })
-    .then((dishes) => {
-        console.log(dishes);
-        return Dishes.remove({});
-    })
-    .then(() => {
-        return mongoose.connection.close();
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+        .then((dish) => {
+            console.log(dish);
+            return Dishes.findByIdAndUpdate(dish._id,
+                { $set: { description: 'Updated test' } },
+                { new: true }).exec();
+        })
+        .then((dish) => {
+            console.log(dish);
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author: 'Lenoardo di Carpaccio'
+            })
+
+            return dish.save()
+        })
+        .then((dish) => {
+            console.log(dish);
+
+            return Dishes.deleteOne({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 
 });
